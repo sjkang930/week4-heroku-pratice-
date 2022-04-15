4237f985119205d03db138d7505f307724844715
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-04-15 15:18:41 -0700

    second commit

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,17 +4,17 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 //mysql://b6fb24cb4c8c37:702be0f5@us-cdbr-east-03.cleardb.com/heroku_a79dfbc72e06757?reconnect=true
 const dbConfigHeroku = {
-	host: "us-cdbr-east-03.cleardb.com",
-	user: "b6fb24cb4c8c37",
-	password: "702be0f5",
-	database: "heroku_a79dfbc72e06757",
+	host: "ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+	user: "ehbx8x9q9lbchsfq",
+	password: "dl10ejdt5t45uqge",
+	database: "z0juvy6zgas4mid0",
 	multipleStatements: false
 };
 
 const dbConfigLocal = {
 	host: "localhost",
 	user: "root",
-	password: "Password",
+	password: "R751956r",
 	database: "database1",
 	multipleStatements: false
 };
